2b5991cf273bad5e1fbb53c8b9f445bc9127920a
specktro <specktro@appetito.mx>
2016-09-02 11:30:37 -0500

    chore: Initial commit with .gitgnore file

---
+++ PokemonLocation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PokemonLocation
+//
+//  Created by specktro on 8/31/16.
+//  Copyright © 2016 specktro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
